060ff30e3da922d937da3e4bc046592636a839df
bryandebourbon <bryandebourbon@gmail.com>
2025-01-01 19:28:52 -0500

    Add/update file Sources/Testpm/Testpm.swiftpm/ContentView.swift

---
+++ Sources/Testpm/Testpm.swiftpm/ContentView.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+public struct ContentView: View {
+    public init() {}  // public initializer
+    public var body: some View {
+        Text("Hello from Testpm’s ContentView!")
+    }
+}
